724cd5a1dd57cdfca716c0aa5aeaa79a9d3b646d
David Kennedy <david.kennedy@workday.com>
2020-12-05 13:52:55 -0800

    day 4 and 5

---
+++ src/bin/day04.rs
@@ -0,0 +1,137 @@
+use std::fs;
+use regex::Regex;
+use std::collections::HashMap;
+
+fn main() {
+    let input: String = fs::read_to_string("src/bin/input04.txt").expect("Could not read file");
+    let total: usize;
+
+    let pairs = Regex::new("(\\S+)").unwrap();
+    let keyval = Regex::new("(\\w+):(.+)").unwrap();
+    let mut passports: Vec<HashMap<String, String>> = Vec::new();
+
+    for obj in input.split("\n\n") {
+        let mut map: HashMap<String, String> = HashMap::new();
+        for pair in pairs.captures_iter(obj) {
+            for derps in keyval.captures_iter(&pair[1]) {
+                map.insert(String::from(&derps[1]), String::from(&derps[2]));
+            }
+        }
+        passports.push(map);
+    }
+
+    total = passports.iter()
+        .filter(|x| x.keys().count() >= 7)
+        .filter(|x| {
+            println!("{:?}", x);
+            check_byr(x) && check_ecl(x) && check_eyr(x) && check_hcl(x) &&
+                check_hgt(x) && check_iyr(x) && check_pid(x)
+        })
+        .count();
+
+    // Part 1
+    // total = passports.iter()
+    //     .filter(|x| { x.keys().count() == 8 || x.keys().count() == 7 && x.get("cid") == None})
+    //     .count();
+
+    println!("{}", total);
+}
+
+fn check_byr(map: &HashMap<String, String>) -> bool {
+    if check_map_val(map, "byr", |val| val_in_range(val, 1920, 2002)) {
+        true
+    }
+    else {
+        println!("  byr has bad val");
+        false
+    }
+}
+
+fn check_iyr(map: &HashMap<String, String>) -> bool {
+    if check_map_val(map, "iyr", |val| val_in_range(val, 2010, 2020)) {
+        true
+    }
+    else {
+        println!("  iyr has bad val");
+        false
+    }
+}
+
+fn check_eyr(map: &HashMap<String, String>) -> bool {
+    if check_map_val(map, "eyr", |val| val_in_range(val, 2020, 2030)) {
+        true
+    }
+    else {
+        println!("  eyr has bad val");
+        false
+    }
+}
+
+fn check_hgt(map: &HashMap<String, String>) -> bool {
+    if check_map_val(map, "hgt", |val| {
+        if val.contains("in") {
+            val_in_range(&val.replace("in", ""), 59, 76)
+        }
+        else {
+            val_in_range(&val.replace("cm", ""), 150, 193)
+        }
+    }) {
+            true
+        }
+        else {
+            println!("  hgt has bad val");
+            false
+        }
+}
+
+fn check_hcl(map: &HashMap<String, String>) -> bool {
+    if check_map_val(map, "hcl", |val| {
+        let r = Regex::new("^#[\\da-f]{6}$").unwrap();
+        r.is_match(val)
+    }) {
+            true
+        }
+        else {
+            println!("  byr has bad val");
+            false
+        }
+}
+
+fn check_ecl(map: &HashMap<String, String>) -> bool {
+    if check_map_val(map, "ecl", |val| {
+        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&val)
+    }) {
+        true
+    }
+    else {
+        println!("  ecl has bad val");
+        false
+    }
+}
+
+fn check_pid(map: &HashMap<String, String>) -> bool {
+    if check_map_val(map, "pid", |val| {
+        val.len() == 9
+    }) {
+        true
+    }
+    else {
+        println!("  pid has bad val");
+        false
+    }
+}
+
+fn check_map_val(map: &HashMap<String, String>, key: &str, fun: impl Fn(&str) -> bool) -> bool {
+    if let Some(val) = map.get(key) {
+        return fun(val);
+    }
+
+    false
+}
+
+fn val_in_range(val: &str, lower: usize, upper: usize) -> bool {
+    let num = val.parse::<usize>().unwrap();
+    println!("    checking val {} between {}-{}", val, lower, upper);
+
+    num >= lower && num <= upper
+}
---
+++ src/bin/day05.rs
@@ -0,0 +1,66 @@
+use std::fs;
+
+fn main() {
+    let input: String = fs::read_to_string("src/bin/input05.txt").expect("Could not read file");
+    let max_seat = input.lines()
+        .map(|l| get_seat_checksum(l))
+        .max()
+        .unwrap();
+
+    println!("{}", max_seat);
+
+    let mut all_seats: Vec<usize> = input.lines()
+        .map(|l| get_seat_checksum(l))
+        .collect::<Vec<usize>>();
+
+    all_seats.sort();
+
+    let mut last: usize = all_seats[0];
+    let mut my_seat: usize = 0;
+
+    // println!("{:?}", all_seats);
+    for seat in &all_seats[1..] {
+        if last != seat - 1 {
+            my_seat = seat - 1;
+            break;
+        }
+        else {
+            last = *seat;
+        }
+    };
+
+    println!("my seat {}", my_seat)
+}
+
+fn get_seat_checksum(pass: &str) -> usize {
+    let row_part = &pass[..7];
+    let col_part = &pass[7..];
+
+    let mut row_min = 0;
+    let mut row_max = 127;
+    for r in row_part.chars() {
+        if r == 'B' {
+            row_min += ((row_max - row_min) + 1) / 2
+        }
+        else {
+            row_max -= ((row_max - row_min) + 1) / 2
+        }
+    }
+    assert_eq!(row_min, row_max);
+
+    let mut col_min = 0;
+    let mut col_max = 7;
+    for c in col_part.chars() {
+        if c == 'R' {
+            col_min += ((col_max - col_min) + 1) / 2
+        }
+        else {
+            col_max -= ((col_max - col_min) + 1) / 2
+        }
+    }
+    assert_eq!(col_min, col_max);
+
+    // println!("row {} col {}", row_min, col_min);
+
+    row_min * 8 + col_min
+}
