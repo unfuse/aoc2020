b0a010ef7cd5d89e9407e148aecd0f6dcbd55c43
David Kennedy <david.kennedy@workday.com>
2020-12-09 07:59:52 -0800

    day 9 complete

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -5,42 +5,18 @@ fn main() {
     let nums: Vec<usize> = fs::read_to_string("src/bin/input09.txt")
         .unwrap().lines().map(|x| x.parse().unwrap()).collect();
 
-//     let nums: Vec<usize> = String::from(r"35
-// 20
-// 15
-// 25
-// 47
-// 40
-// 62
-// 55
-// 65
-// 95
-// 102
-// 117
-// 150
-// 182
-// 127
-// 219
-// 299
-// 277
-// 309
-// 576").lines().map(|x| x.parse().unwrap()).collect();
-    let preamble = 25;
-
     // Part 1
-    // For my data set, while logging results, i noticed this index came up many times and it's the right answer, but my program never terminates
-    // println!("{}", nums[508]);
-    // let max_index = find_error(preamble, &nums, preamble);
-    // println!("{} => {}", max_index, nums[max_index]);
+    let bad_index: usize = find_error(25, &nums, 25);
+    let bad_value: usize = nums[bad_index];
+    println!("{} => {}", bad_index, bad_value);
 
     // Part 2
-    let (low, high) = find_longest_chain_for_sum(nums[508], &nums);
+    let (low, high) = find_longest_chain_for_sum(bad_value, &nums);
     let mut nums_copy: Vec<usize>= nums[low..=high].iter().copied().collect();
     nums_copy.sort_unstable();
     println!("indices {} and {} values sum to {}", low, high, nums_copy.first().unwrap() + nums_copy.last().unwrap());
 }
 
-// Doesn't terminate for real problem, debug logging told me what the answer was
 fn find_error(index: usize, nums: &[usize], preamble: usize) -> usize {
     if index >= nums.len() {
         panic!("got to the end of the list??");
@@ -49,10 +25,6 @@ fn find_error(index: usize, nums: &[usize], preamble: usize) -> usize {
     let start: usize = index - preamble;
     let value: usize = nums[index];
     let range: &[usize] = &nums[start..index];
-    let mut max_index: usize = index;
-
-    // println!("find sums for {} => {}", index, value);
-    // println!("for index {} with preamble {} consider ranges ({}, {}) x ({}, {})", index, preamble, start, index - 1, start + 1, index);
 
     for (i, &i_val) in range.iter().enumerate() {
         if i_val >= value { continue }
@@ -61,13 +33,12 @@ fn find_error(index: usize, nums: &[usize], preamble: usize) -> usize {
             if j_val >= value || j_val == i_val { continue }
 
             if i_val + j_val == value {
-                // println!("  found possible state: {} {}", i_val, j_val);
-                max_index = max(max_index, find_error(index + 1, nums, preamble));
+                return max(index, find_error(index + 1, nums, preamble));
             }
         }
     }
 
-    max_index
+    index
 }
 
 fn find_longest_chain_for_sum(target: usize, nums: &[usize]) -> (usize, usize) {
